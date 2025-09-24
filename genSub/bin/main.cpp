dd9b9d00ca51e4b05ee103ba9b744834ffefe330
Tommy To <beast2hk-general@yahoo.com>
2025-09-24 10:17:33 +0900

    First commit

---
+++ genSub/bin/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  genSub
+//
+//  Created by Tommy To on 2025/09/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
